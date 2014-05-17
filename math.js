d26b8e5fb8c7a19266e1e855b20a6eba6dffeb4a
Noah Moroze <noahm@moroze.com>
2014-05-17 18:54:37 -0400

    Gray line

--- math.js
+++ math.js
@@ -13,3 +13,22 @@ function randInt(min, max) {
 	return Math.floor((Math.random() * max) + min);
 }
 
+//http://stackoverflow.com/questions/15397036/drawing-dashed-lines-on-html5-canvas
+CanvasRenderingContext2D.prototype.dashedLine = function (x1, y1, x2, y2, dashLen) {
+    if (dashLen == undefined) dashLen = 2;
+    this.moveTo(x1, y1);
+
+    var dX = x2 - x1;
+    var dY = y2 - y1;
+    var dashes = Math.floor(Math.sqrt(dX * dX + dY * dY) / dashLen);
+    var dashX = dX / dashes;
+    var dashY = dY / dashes;
+
+    var q = 0;
+    while (q++ < dashes) {
+        x1 += dashX;
+        y1 += dashY;
+        this[q % 2 == 0 ? 'moveTo' : 'lineTo'](x1, y1);
+    }
+    this[q % 2 == 0 ? 'moveTo' : 'lineTo'](x2, y2);
+};
