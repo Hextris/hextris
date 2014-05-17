b13a649539625620a29b0cd35061ddf884dbceed
Michael Yang <themichaelyang@gmail.com>
2014-05-17 12:50:12 -0400

    noah is kewl

--- entities.js
+++ entities.js
@@ -1,16 +1,22 @@
 //you can change these to sexier stuff
 var colors = [
-	"black",
-	"orange", 
-	"red",
-	"blue",
+"black",
+"orange", 
+"red",
+"blue",
 ];
 
-var Clock = function(sides) {
+var Clock = function(sideLength) {
 	this.position = 0;
-	this.sides = sides;
+	this.sides = 6;
 	this.blocks = [];
-	for(var i=0; i<sides; i++) {
+	this.angle = 30;
+	this.sideLength = sideLength;
+	this.strokeColor = 'black';
+	this.x = canvas.width / 2;
+	this.y = canvas.height / 2;
+
+	for(var i=0; i< this.sides; i++) {
 		this.blocks.push([]);
 	}
 
@@ -19,7 +25,31 @@ var Clock = function(sides) {
 	}
 
 	this.rotate = function(steps) {
-		this.position += steps;
+		this.positione += steps;
 		this.position = Math.abs(((this.position%sides)+this.position) % sides);
+	}
+
+	this.draw = function() {
+		this.drawPolygon(this.x, this.y, this.sides, this.sideLength, this.angle);
+	}
+
+	this.drawPolygon = function(x, y, sides, radius, theta) { // can make more elegant, reduce redundancy, fix readability
+		ctx.beginPath();
+		var coords = rotatePoint(0, radius, theta);
+		ctx.moveTo(coords.x + x, coords.y + y);
+		var oldX = coords.x;
+		var oldY = coords.y;
+		for (var i = 0; i < sides; i++) {
+			coords = rotatePoint(oldX, oldY, 360 / sides); 
+			ctx.lineTo(coords.x + x, coords.y + y);
+			ctx.moveTo(coords.x + x, coords.y + y);
+			oldX = coords.x;
+			oldY = coords.y;
+			// console.log(coords);
+		}
+		ctx.closePath();
+		// ctx.fill();
+		ctx.strokeStyle = this.strokeColor;
+		ctx.stroke();
 	}
 }
