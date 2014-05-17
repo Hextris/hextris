9fc709fb9c45880d3e10c96b9830f3e4ebf37f59
Michael Yang <themichaelyang@gmail.com>
2014-05-17 18:34:02 -0400

    added start and end screens

--- input.js
+++ input.js
@@ -7,3 +7,12 @@ keypress.register_combo({
 	keys: "right", 
 	on_keyup: function(){MainClock.rotate(-1)},
 });
+
+keypress.register_combo({
+	keys: "enter", 
+	on_keyup: function(){
+		if (gameState != 1) {
+			gameState = 1;
+		}
+	},
+});
