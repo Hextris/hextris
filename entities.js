dafc21cd2b9da81c3fb7d3890cc781dff0e0923f
meadowstream <meadowstream@users.noreply.github.com>
2014-05-17 16:48:55 -0400

    sort of fixing collision detection

--- entities.js
+++ entities.js
@@ -23,16 +23,17 @@ function Block(lane, color, distFromHex, settled) {
 		this.distFromHex = distFromHex;
 	}
 	else {
-		this.distFromHex = 300;
+		this.distFromHex = 470;
 	}
 	this.draw = function() {
-		this.width = 2 * this.distFromHex / Math.sqrt(3) + this.height;
+		this.width = 2 * this.distFromHex / Math.sqrt(3);
+		this.widthswag = this.width + this.height + 5;
 		var p1 = rotatePoint(-this.width/2, this.height/2, this.angle);
 		var p2 = rotatePoint(this.width/2, this.height/2, this.angle);
-		var p3 = rotatePoint(this.width/2, -this.height/2, this.angle);
-		var p4 = rotatePoint(-this.width/2, -this.height/2, this.angle);
+		var p3 = rotatePoint(this.widthswag/2, -this.height/2, this.angle);
+		var p4 = rotatePoint(-this.widthswag/2, -this.height/2, this.angle);
 		
-		ctx.fillStyle="#FF0000";
+		ctx.fillStyle=this.color;
 		var baseX = canvas.width/2 + Math.sin((this.angle) * (Math.PI/180)) * (this.distFromHex + this.height/2);
 		var baseY = canvas.height/2 - Math.cos((this.angle) * (Math.PI/180)) * (this.distFromHex + this.height/2);
 
@@ -45,6 +46,7 @@ function Block(lane, color, distFromHex, settled) {
 		ctx.closePath();
 		ctx.fill();
 	};
+
 }
 
 var Clock = function(sideLength) {
@@ -64,22 +66,29 @@ var Clock = function(sideLength) {
 	this.addBlock = function(block) {
 		block.settled = 1;
 		var lane = 0;
-		lane += this.position + block.lane;
-		lane = lane % this.sides;
+		lane += this.position - block.lane;
 		while(lane < 0) {
 			lane = lane + this.sides;
 		}
+		lane = lane % this.sides;
 		block.distFromHex = MainClock.sideLength / 2 * Math.sqrt(3) + block.height * this.blocks[lane].length;
 		this.blocks[lane].push(block);
 		// consolidateBlocks(this,lane,this.blocks.length-1);
 	};
 
-	this.doesBlockCollide = function(block, iter, index) {
+	this.doesBlockCollide = function(block, iter) {
 		if (block.settled) return;
-		var arr = this.blocks[(block.lane + this.position % this.sides) % this.sides];
-		var thisIn = index === undefined ? arr.length - 1 : index - 1;
+
+		var lane = block.lane - this.position;
+
+		while (lane < 0) {
+			lane += 6;
+		}
+		lane = lane % this.sides;
+
+		var arr = this.blocks[lane];
 		if (arr.length > 0) {
-			if (block.distFromHex + iter - arr[thisIn].distFromHex - arr[thisIn].height <= 0) {
+			if (block.distFromHex + iter - arr[arr.length - 1].distFromHex - arr[arr.length - 1].height <= 0) {
 				this.addBlock(block);
 			}
 		}
@@ -93,22 +102,19 @@ var Clock = function(sideLength) {
 	};
 
 	this.rotate = function(steps) {
-		var oldPosition = this.position;
 		this.position += steps;
+		while (this.position < 0) {
+			this.position += 6;
+		}
+
 		this.position = this.position % this.sides;
-		while(this.position < 0) {
-			this.position = this.position + this.sides;
-		}
-		for(var i=0; i<this.blocks.length; i++) {
-			for(var j=0; j<this.blocks[i].length; j++) {
-				this.blocks[i][j].lane += (this.position - oldPosition);
-				this.blocks[i][j].lane = this.blocks[i][j].lane % this.sides;
-				while(this.blocks[i][j].lane < 0) {
-					this.blocks[i][j].lane += this.sides;
-				}
-			}
-		}
-		this.angle = 30 + this.position * 60;
+		this.blocks.forEach(function(blocks){
+			blocks.forEach(function(block){
+				block.angle = block.angle - steps * 60;
+			});
+		})
+
+		this.angle = this.angle + steps * 60;
 	};
 
 	this.draw = function() {
