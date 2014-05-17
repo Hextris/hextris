e632275bb2c891cf80c8d54b684727cc9607658c
lengstrom <lengstrom@users.noreply.github.com>
2014-05-17 10:22:40 -0400

    added main loop + render loop + animFrame

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 // main thing here
+
+window.requestAnimFrame = (function(){
+	return window.requestAnimationFrame 		||
+		window.webkitRequestAnimationFrame	||
+		window.mozRequestAnimationFrame		||
+		function( callback ){
+			window.setTimeout(callback, 1000 / 60);
+		};
+})();
+
+(function animloop(){
+  requestAnimFrame(animloop);
+  render();
+})();
+
+function render() {
+	// game code
+
+	requestAnimFrame(animloop);
+}
