39d0d52878b600bdf9d54d36730a3b9e63577067
meadowstream <meadowstream@users.noreply.github.com>
2014-05-17 10:52:25 -0400

    added blocks + tests for blocks - not sure if works yet

--- main.js
+++ main.js
@@ -11,16 +11,43 @@ window.requestAnimFrame = (function(){
 		};
 })();
 
+var blocks = [];
+
+for (var i = 0; i < 6, i++) {
+	blocks.push(new Block(i, 'green'));
+}
+
 (function animloop(){
   requestAnimFrame(animloop);
   render();
 })();
 
-function render() {
+function Render() {
 	// game code
-	requestAnimFrame(render);
+	blocks.forEach(function(o){
+		o.draw();
+	})
+
+	ctx.fillRect(200, 200, 200, 200);
 }
 
-function block(lane) {
+function Block(lane, color, time) {
+	this.lane = lane;
+	this.angle = 15 * (Math.PI / 180) + 30 * (Math.PI / 180) * lane;
+	this.color = color;
 
+	this.draw = function() {
+		ctx.translate(canvas.width / 2, canvas.height / 2);
+		ctx.rotate(this.angle);
+		ctx.fillStyle = color;
+		ctx.fillRect(canvas.width/2 + Math.cos(this.angle) * time, canvas.height/2 + Math.sin(this.angle) * time, 70, 30);
+		ctx.restore();
+	};
+
+	if (!time) {
+		this.time = time;
+	}
+	else {
+		time = 200;
+	}
 }
