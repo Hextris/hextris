030c4c0daaf1eb6ee0c9b0c6290019dc59d7c7e6
garrettdreyfus <garrettdreyfus@gmail.com>
2014-05-18 11:43:49 -0400

    touch smi-working

--- input.js
+++ input.js
@@ -17,7 +17,19 @@ keypress.register_combo({
 		}
 	},
 });
+var tapLeft = Hammer(document.getElementById("leftTap")).on("tap", function(event) {
+	if (gameState != 1) {
+		init();
+	}
+	MainClock.rotate(1)
 
+});
+var tapRight  = Hammer(document.getElementById("rightTap")).on("tap", function(event) {
+	if (gameState != 1) {
+		init();
+	}
+	MainClock.rotate(-1)
+});
 keypress.register_combo({
 	keys: "space", 
 	on_keyup: function(){
