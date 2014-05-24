d61f35e64d036c89c90055af35b20fc9b2f83622
lengstrom <lengstrom@users.noreply.github.com>
2014-05-24 14:41:14 -0400

    added fade in for hexagonal boundary

--- js/main.js
+++ js/main.js
@@ -192,16 +192,28 @@ function render() {
 	}
 
 	MainClock.draw();
-	drawPolygon(canvas.originalWidth / 2 , canvas.originalHeight / 2 , 6, 220, 30, '#BDA0CB', false,6);
+	if (gameState == -2) {
+		if (Date.now() - startTime > 1300) {
+			var op = (Date.now() - startTime - 1300)/500;
+			debugger;
+			ctx.globalAlpha = op;
+			drawPolygon(canvas.originalWidth / 2 , canvas.originalHeight / 2 , 6, 220, 30, "#bda0cb", false,6);
+			ctx.globalAlpha = 1;
+		}
+	} else {
+		drawPolygon(canvas.originalWidth / 2 , canvas.originalHeight / 2 , 6, 220, 30, '#BDA0CB', false,6);
+	}
+	
 }
 
 function stepInitialLoad() {
-	var dur = 1300;
-	var dy = getStepDY(Date.now() - startTime, 0, 100 + canvas.height/2, dur);
-	if (Date.now() - startTime > dur) {
+	var dy = getStepDY(Date.now() - startTime, 0, 100 + canvas.height/2, 1300);
+	if (Date.now() - startTime > 1300) {
 		MainClock.dy = 0;
 		MainClock.y = canvas.height/2;
-		gameState = 1;
+		if (Date.now() - startTime - 500 > 1300) {
+			gameState = 1;
+		}
 	} else {
 		MainClock.dy = dy;
 	}
