d6b4716bd04b5bea54562e3a03f6ecf8c4b8b9a7
Michael Yang <themichaelyang@gmail.com>
2014-05-26 01:11:47 -0400

    added clock hand

--- js/render.js
+++ js/render.js
@@ -1,3 +1,5 @@
+var grey = '#bdc3c7';
+
 function render() {
 	ctx.clearRect(0, 0, trueCanvas.width, trueCanvas.height);
 	clearGameBoard();
@@ -9,11 +11,11 @@ function render() {
 				op = 1;
 			}
 			ctx.globalAlpha = op;
-			drawPolygon(trueCanvas.width / 2 , trueCanvas.height / 2 , 6, (settings.rows * settings.blockHeight) * (2/Math.sqrt(3)) + settings.hexWidth, 30, "#bdc3c7", false,6);
+			drawPolygon(trueCanvas.width / 2 , trueCanvas.height / 2 , 6, (settings.rows * settings.blockHeight) * (2/Math.sqrt(3)) + settings.hexWidth, 30, grey, false,6);
 			ctx.globalAlpha = 1;
 		}
 	} else {
-		drawPolygon(trueCanvas.width / 2 + gdx, trueCanvas.height / 2 + gdy, 6, (settings.rows * settings.blockHeight) * (2/Math.sqrt(3)) + settings.hexWidth, 30, '#bdc3c7', false, 6);
+		drawPolygon(trueCanvas.width / 2 + gdx, trueCanvas.height / 2 + gdy, 6, (settings.rows * settings.blockHeight) * (2/Math.sqrt(3)) + settings.hexWidth, 30, grey, false, 6);
 	}
 
 	for (var i in MainClock.blocks) {
