75ff870065d0894dd8856ff200650b0fb5c90d46
lengstrom <lengstrom@users.noreply.github.com>
2014-06-24 09:42:38 -0400

    fixed restart method not clearing the screen, started fixing  distFromHex changing after restart

--- js/save-state.js
+++ js/save-state.js
@@ -3,7 +3,13 @@ function exportSaveState() {
 	var state = {};
 	
 	if(gameState == 1 || gameState == -1 || (gameState == 0 && localStorage.getItem('saveState') !== undefined)) {
-        MainClock.blocks.map(function(block){block.distFromHex /= settings.scale});
+        
+        MainClock.blocks.forEach(function(o){
+			o.forEach(function(b){
+				b.distFromhex /= settings.scale;
+			})
+        });
+
 		blocks.map(function(block){block.distFromHex /= settings.scale})
 		state = {
 			clock: MainClock,
