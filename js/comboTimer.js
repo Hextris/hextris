54a4759b9c559bc217727c60aa88cde87179db0b
meadowstream <meadowstream@users.noreply.github.com>
2014-06-27 22:09:36 -0400

    combo lines now match the faded blocks during menu screen play

--- js/comboTimer.js
+++ js/comboTimer.js
@@ -18,20 +18,24 @@ function drawSide(startVertex,endVertex,fraction){
     endVertex = endVertex%6;
     ctx.globalAlpha=1;
     ctx.beginPath();
-    ctx.lineCap = "round"
-    ctx.strokeStyle=MainClock.lastColorScored;
+    ctx.lineCap = "round";
+    if (gameState === 0) {
+        ctx.strokeStyle = hexColorsToTintedColors[MainClock.lastColorScored];
+    } else {
+        ctx.strokeStyle = MainClock.lastColorScored;
+    }
     ctx.lineWidth =4;
     var radius = (settings.rows * settings.blockHeight) * (2/Math.sqrt(3)) + settings.hexWidth + 2;
     var halfRadius = radius/2;
     var triHeight = radius *(Math.sqrt(3)/2);
     var Vertexes =[[halfRadius,triHeight],[radius,0],[halfRadius,-triHeight],[-halfRadius,-triHeight],[-radius,0],[-halfRadius,triHeight]].reverse();
-    var startX =trueCanvas.width/2 + Vertexes[startVertex][0]; 
-    var startY =trueCanvas.height/2 + Vertexes[startVertex][1]; 
+    var startX =trueCanvas.width/2 + Vertexes[startVertex][0];
+    var startY =trueCanvas.height/2 + Vertexes[startVertex][1];
     var endX = trueCanvas.width/2 + Vertexes[endVertex][0];
     var endY = trueCanvas.height/2 + Vertexes[endVertex][1];
     ctx.moveTo(startX,startY);
     ctx.lineTo(((endX-startX)*fraction)+startX,((endY-startY)*fraction)+startY);
-    ctx.closePath()
-    ctx.fill()
+    ctx.closePath();
+    ctx.fill();
     ctx.stroke();
-} 
+}
