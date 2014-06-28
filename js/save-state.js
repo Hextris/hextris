0e29354d029315b52998f3468895a241e46215e3
meadowstream <meadowstream@users.noreply.github.com>
2014-06-27 21:19:57 -0400

    fixed initial setup not rendering correctly in menu

--- js/save-state.js
+++ js/save-state.js
@@ -1,16 +1,13 @@
 function exportSaveState() {
 	console.log('o')
+	debugger;
 	var state = {};
-	
-	if(gameState == 1 || gameState == -1 || (gameState == 0 && localStorage.getItem('saveState') !== undefined)) {
-        
-        MainClock.blocks.forEach(function(o){
-			o.forEach(function(b){
-				b.distFromhex /= settings.scale;
-			})
-        });
 
-		blocks.map(function(block){block.distFromHex /= settings.scale})
+	if(gameState == 1 || gameState == -1 || (gameState === 0 && localStorage.getItem('saveState') !== undefined)) {
+		console.log(MainClock.blocks[5][0].distFromHex);
+		MainClock.blocks.map(function(i){i.map(function(o){o.distFromHex /= settings.scale})});
+		console.log(MainClock.blocks[5][0].distFromHex);
+		blocks.map(function(block){block.distFromHex /= settings.scale;});
 		state = {
 			clock: MainClock,
 			blocks: blocks,
@@ -19,7 +16,7 @@ function exportSaveState() {
 			gdx: gdx,
 			gdy: gdy,
 			comboMultiplier:comboMultiplier
-		}
+		};
 	}
 
 	return JSONfn.stringify(state);
