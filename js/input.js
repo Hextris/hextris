93b8e4276a8d92f0a75668d735842812939d638f
meadowstream <meadowstream@users.noreply.github.com>
2014-06-29 14:35:10 -0400

    Removed spacebar speedup

--- js/input.js
+++ js/input.js
@@ -86,16 +86,6 @@ keypress.register_combo({
 });
 
 keypress.register_combo({
-    keys: "space",
-    on_keydown: function() {
-        spaceModifier = 2;
-    },
-    on_keyup: function() {
-        spaceModifier = 1;
-    }
-});
-
-keypress.register_combo({
     keys: "enter",
     on_keydown: function() {
         if (gameState==2 || gameState==1 || importing == 1) {
