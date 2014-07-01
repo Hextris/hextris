d8420a753e28b925a4d0e15c52029a637e9ee3a3
Garrett Finucane <garrettdreyfus@gmail.com>
2014-07-01 11:02:52 -0400

    no more -2 gameState

--- js/view.js
+++ js/view.js
@@ -25,6 +25,18 @@ var rgbColorsToTintedColors = {
 var hexagonBackgroundColor = 'rgb(236, 240, 241)';
 var hexagonBackgroundColorClear = 'rgba(236, 240, 241, 0.5)';
 var centerBlue = 'rgb(44,62,80)'; //tumblr?
+var angularVelocityConst = 4;
+
+// t: current time, b: begInnIng value, c: change In value, d: duration
+function easeOutCubic(t, b, c, d) {
+	return c*((t=t/d-1)*t*t + 1) + b;
+}
+
+var colorSounds =  {"#e74c3c": new Audio("../sounds/lowest.ogg"),
+"#f1c40f":new Audio("../sounds/highest.ogg"),
+"#3498db":new Audio("../sounds/middle.ogg"),
+	"#2ecc71":new Audio("../sounds/highest.ogg") //fix this later
+};
 
 function renderText(x, y, fontSize, color, text, font) {
     if (!font) {
