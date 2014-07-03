03692b426ec268a6be1d4f0bb399f1f487b783a6
meadowstream <meadowstream@users.noreply.github.com>
2014-07-03 19:07:51 -0400

    added custom fonts to Text() object

--- js/Text.js
+++ js/Text.js
@@ -7,9 +7,9 @@ function Text(x,y,text,font,color,incrementFunction){
 	this.text = text;
 	this.alive=1;
 	this.draw = function(){
-		if(this.alive>0){
+		if (this.alive>0) {
 			ctx.globalAlpha = this.opacity;
-                        renderText((this.x + gdx), (this.y + gdy),36,this.color,this.text);
+            renderText((this.x + gdx), (this.y + gdy),36,this.color,this.text,this.font);
 			ctx.globalAlpha =1;
 			incrementFunction(this);
 			return true;
