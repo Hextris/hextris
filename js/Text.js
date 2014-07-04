1291e53d2a3d68131f492800ccfa5645b090b338
Garrett Finucane <garrettdreyfus@gmail.com>
2014-07-04 09:46:14 -0400

    Fixed the small text

--- js/Text.js
+++ js/Text.js
@@ -9,7 +9,7 @@ function Text(x,y,text,font,color,incrementFunction){
 	this.draw = function(){
 		if (this.alive>0) {
 			ctx.globalAlpha = this.opacity;
-            renderText((this.x + gdx), (this.y + gdy),36,this.color,this.text,this.font);
+                        renderText((this.x + gdx), (this.y + gdy),40,this.color,this.text);
 			ctx.globalAlpha =1;
 			incrementFunction(this);
 			return true;
