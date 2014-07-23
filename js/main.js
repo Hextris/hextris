ca512a1524ca0d3099dd87effc654217e1260930
lengstrom <lengstrom@users.noreply.github.com>
2014-07-23 09:30:39 -0400

    removed useless function + removed alert

--- js/main.js
+++ js/main.js
@@ -201,19 +201,6 @@ function setStartScreen() {
 	requestAnimFrame(animLoop);
 }
 
-//t: current time, b: begInnIng value, c: change In value, d: duration
-function getStepDY(t, b, c, d) {
-	if ((t/=d) < (1/2.75)) {
-		return c*(7.5625*t*t) + b;
-	} else if (t < (2/2.75)) {
-		return c*(7.5625*(t-=(1.5/2.75))*t + 0.75) + b;
-	} else if (t < (2.5/2.75)) {
-		return c*(7.5625*(t-=(2.25/2.75))*t + 0.9375) + b;
-	} else {
-		return c*(7.5625*(t-=(2.625/2.75))*t + 0.984375) + b;
-	}
-}
-
 function animLoop() {
 	switch (gameState) {
 		case 1:
