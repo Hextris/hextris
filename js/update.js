a59a6e1d3e4f5eea21f7938aa5060de5fdd7a2b8
lengstrom <lengstrom@users.noreply.github.com>
2014-08-21 00:37:25 -0400

    made game time based. made game slightly easier

--- js/update.js
+++ js/update.js
@@ -1,10 +1,12 @@
+
 //remember to update history function to show the respective iter speeds
-function update() {
-	var now = MainHex.ct;
+function update(dt) {
+	console.log('dt: ' + dt);
+	MainHex.dt = dt;
 	if (gameState == 1) {
 		waveone.update();
-		if (now - waveone.prevTimeScored > 1000) {
-			waveone.prevTimeScored = now;
+		if (MainHex.ct - waveone.prevTimeScored > 1000) {
+			waveone.prevTimeScored = MainHex.ct;
 		}
 	}
 	var lowestDeletedIndex = 99;
@@ -16,7 +18,7 @@ function update() {
 	for (i = 0; i < blocks.length; i++) {
 		MainHex.doesBlockCollide(blocks[i]);
 		if (!blocks[i].settled) {
-			if (!blocks[i].initializing) blocks[i].distFromHex -= blocks[i].iter * settings.scale;
+			if (!blocks[i].initializing) blocks[i].distFromHex -= blocks[i].iter * dt * settings.scale;
 		} else if (!blocks[i].removed) {
 			blocks[i].removed = 1;
 		}
@@ -24,12 +26,12 @@ function update() {
 
 	for (i = 0; i < MainHex.blocks.length; i++) {
 		for (j = 0; j < MainHex.blocks[i].length; j++) {
-            if (MainHex.blocks[i][j].checked ==1 ) {
-                consolidateBlocks(MainHex,MainHex.blocks[i][j].attachedLane,MainHex.blocks[i][j].getIndex());
-                MainHex.blocks[i][j].checked=0;
-            }
-        }
-    }
+			if (MainHex.blocks[i][j].checked ==1 ) {
+				consolidateBlocks(MainHex,MainHex.blocks[i][j].attachedLane,MainHex.blocks[i][j].getIndex());
+				MainHex.blocks[i][j].checked=0;
+			}
+		}
+	}
 
 	for (i = 0; i < MainHex.blocks.length; i++) {
 		lowestDeletedIndex = 99;
@@ -56,7 +58,7 @@ function update() {
 			MainHex.doesBlockCollide(block, j, MainHex.blocks[i]);
 
 			if (!MainHex.blocks[i][j].settled) {
-				MainHex.blocks[i][j].distFromHex -= block.iter * settings.scale;
+				MainHex.blocks[i][j].distFromHex -= block.iter * dt * settings.scale;
 			}
 		}
 	}
@@ -67,5 +69,7 @@ function update() {
 			i--;
 		}
 	}
-        MainHex.ct++;
+
+	MainHex.ct += dt;
+	console.log(MainHex.ct);
 }
