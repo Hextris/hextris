e959fa75c2a58dc1bd6e1ffc104dfe6887274a21
Logan Engstrom <meadowstream@users.noreply.github.com>
2014-08-23 13:15:54 -0400

    made game harder

--- js_v7/wavegen.js
+++ js_v7/wavegen.js
@@ -26,7 +26,7 @@ function waveGen(hex) {
     this.dt = 0;
     this.update = function() {
         this.currentFunction();
-        this.dt = (settings.platform == 'mobile' ? 10.0 : 15) * MainHex.ct;
+        this.dt = 16.66667 * MainHex.ct;
         this.computeDifficulty();
         if ((this.dt - this.lastGen) * settings.creationSpeedModifier > this.nextGen) {
             if (this.nextGen > 600) {
