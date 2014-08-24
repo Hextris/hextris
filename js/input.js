3eaa7a9a2cfb424a6a1a179406019751e26707e0
Garrett Finucane <garrettdreyfus@gmail.com>
2014-08-24 12:40:50 -0400

    added hit detection in inner hexagon

--- js/input.js
+++ js/input.js
@@ -117,13 +117,46 @@ function addKeyListeners() {
         });
     }
 }
+function inside (point, vs) {
+    // ray-casting algorithm based on
+    // http://www.ecse.rpi.edu/Homepages/wrf/Research/Short_Notes/pnpoly.html
+    
+    var x = point[0], y = point[1];
+    
+    var inside = false;
+    for (var i = 0, j = vs.length - 1; i < vs.length; j = i++) {
+        var xi = vs[i][0], yi = vs[i][1];
+        var xj = vs[j][0], yj = vs[j][1];
+        
+        var intersect = ((yi > y) != (yj > y))
+            && (x < (xj - xi) * (y - yi) / (yj - yi) + xi);
+        if (intersect) inside = !inside;
+    }
+    
+    return inside;
+};
 
 function handleClickTap(x,y) {
     if (x < 120 && y < 50 && $('.helpText').is(':visible')) {
         showHelp();
         return;
     }
-
+    var radius = settings.hexWidth ;
+    var halfRadius = radius/2;
+    var triHeight = radius *(Math.sqrt(3)/2);
+    var Vertexes =[
+        [radius,0],
+        [halfRadius,-triHeight],
+        [-halfRadius,-triHeight],
+        [-radius,0],
+        [-halfRadius,triHeight],
+        [halfRadius,triHeight]];
+    Vertexes = Vertexes.map(function(coord){ 
+        return [coord[0] + trueCanvas.width/2, coord[1] + trueCanvas.height/2]});
+    if (gameState == 1 && inside([x,y],Vertexes)){
+        //SET SPEED FAST
+        return;
+    }
     if (gameState == 2 && canRestart) {
         setTimeout(function() {
             if(tweetblock) {
