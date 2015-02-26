13c51e1aaed59b329573242b8093fb87cb9ac0ed
Garrett Finucane <garrettdreyfus@gmail.com>
2015-02-25 20:37:19 -0500

    app upsells

--- js_v9/main.js
+++ js_v9/main.js
@@ -66,19 +66,20 @@ function hideUIElements() {
 }
 
 function init(b) {
+    if(settings.ending_block==true && b == 1){return;}
     if (b) {
-        hidebottombar();
+            hidebottombar();
 
 
-        if ($('#helpScreen').is(":visible")) {
-            $('#helpScreen').fadeOut(150, "linear");
-        }
+            if ($('#helpScreen').is(":visible")) {
+                $('#helpScreen').fadeOut(150, "linear");
+            }
 
-        setTimeout(function() {
-            $('.helpText').fadeOut(150, "linear");
-                infobuttonfading = false;
-        }, 7000);
-        clearSaveState();
+            setTimeout(function() {
+                $('.helpText').fadeOut(150, "linear");
+                    infobuttonfading = false;
+            }, 7000);
+            clearSaveState();
     }
 
     infobuttonfading = true;
